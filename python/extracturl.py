f65082c1fbccab62e0a53d8bcb635527a7f15695
Jauder Ho <jauderho+git@gmail.com>
2010-05-20 19:31:31 -0700

    added extracturl.py fragment

---
+++ python/extracturl.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#
+# extract urls from a file
+#
+
+import sys,re,urllib,HTMLParser
+from BeautifulSoup import BeautifulSoup
+from xml.dom import minidom
+
+URL = 'file:///Users/codecrumb/Downloads/delicious.htm'
+
+html = urllib.urlopen(URL).read()
+soup = BeautifulSoup(html)
+for link in soup.findAll('a'):
+	print link['href']
